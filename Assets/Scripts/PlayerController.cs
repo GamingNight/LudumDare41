502c58eddbc63838fbe5ea37acd8df0164b7f212
JFAURE <julien.y.faure@gmail.com>
2018-04-21 13:13:53 +0200

    movement improve
    movment improve

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,32 +5,48 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float strength;
-    public float runCoeff;
+    public float boost;
+    public float stamina;
+    public float coco;
 
     private Rigidbody2D rgbd;
-    private Vector3 movement;
+    private Vector2 movement;
     private float drag;
     private float i;
+    private float iStamina;
+    private float indivBoost;
 
     // Use this for initialization
     void Start () {
         rgbd = GetComponent<Rigidbody2D>();
-        drag = rgbd.drag;
-        i = 0;
+        iStamina = stamina;
+        strength = strength / Mathf.Pow(coco, 1);
+        rgbd.mass = rgbd.mass / Mathf.Pow(coco, -1);
+        indivBoost = stamina / 10;
 
     }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        if (Input.GetKey(KeyCode.V))
+        if (Input.GetKey(KeyCode.V) && iStamina != 2000 && indivBoost >0)
         {
             //Handle dash (interrupt walking)
             Dash();
+            indivBoost = indivBoost - 1;
+            if (indivBoost == stamina/10 - 1)
+            {
+                iStamina = iStamina - 1;
+            }
+            //Debug.Log(iStamina);
+        }
+        if (Input.GetKey(KeyCode.V)==false)
+        {
+            indivBoost = stamina / 10;
         }
         //Handle regular walking
         Move();
-
-
+        Debug.Log(rgbd.velocity.magnitude);
+        
     }
 
     private void Move()
@@ -40,7 +56,7 @@ public class PlayerController : MonoBehaviour {
         float vertical = Input.GetAxisRaw("Vertical");
         if (horizontal != 0 || vertical != 0)
         {
-            movement.Set(horizontal, vertical, 0f);
+            movement.Set(horizontal, vertical);
             rgbd.AddForce(movement.normalized * strength);
         }
         //    if (vertical < 0)
@@ -75,8 +91,9 @@ public class PlayerController : MonoBehaviour {
         float vertical = Input.GetAxisRaw("Vertical");
         if (horizontal != 0 || vertical != 0)
         {
-            movement.Set(horizontal, vertical, 0f);
-            rgbd.AddForce(movement.normalized * strength * Mathf.Sqrt(runCoeff));
+            movement.Set(horizontal, vertical);
+//            rgbd.velocity = rgbd.velocity + movement.normalized * boost;
+            rgbd.AddForce(movement.normalized * strength * boost);
         }
         //    if (vertical < 0)
         //    {
