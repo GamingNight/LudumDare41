11f9e6dc7ce8023dc2012a6461e1de3f2741f778
JFAURE <julien.y.faure@gmail.com>
2018-04-21 21:05:21 +0200

    PassesInfinies

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallController : MonoBehaviour {
+public class BallController : MonoBehaviour
+{
 
     public GameObject player;
     public float speed;
@@ -10,45 +11,48 @@ public class BallController : MonoBehaviour {
 
     private bool pass;
     private Rigidbody2D rgbd;
-    private bool dragBall=false;
+    private bool dragBall = false;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         rgbd = GetComponent<Rigidbody2D>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("A dragball update = " + dragBall);
         if (Input.GetKey(KeyCode.Space) == true)
         {
-            pass=true;
+            pass = true;
             float horizontal = Input.GetAxisRaw("Horizontal");
             float vertical = Input.GetAxisRaw("Vertical");
-            rgbd.velocity = speed*new Vector2 (horizontal,vertical);
-            player.GetComponent<PlayerController>().enabled = false;
+            rgbd.velocity = speed * new Vector2(horizontal, vertical); // la balle est lancée
+            player.GetComponent<PlayerController>().enabled = false; // le player n'est plus le joueur anymore
+            GetComponent<BallMagnetism>().enabled = false;// la balle n'est plus aimantée à ce player
         }
         else
         {
             pass = false;
         }
-        if (pass == true)
-        {
-            GetComponent<BallMagnetism>().enabled = false;
-        }
-        if (dragBall == true) { rgbd.drag = rgbd.drag + slowBall * Time.deltaTime; }
-	}
+        if (dragBall == true) { rgbd.drag = rgbd.drag + slowBall * Time.deltaTime; }// la balle est freinée dans le child du receveur
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
-            GetComponent<BallMagnetism>().enabled = true;
+            GetComponent<BallMagnetism>().enabled = true; //le receveur est aimanté à la balle
+            dragBall = false; // le freinage de la balle est éteint
+            rgbd.drag = 0; //idem
         }
-        if (other.gameObject.tag == "Ball")
+        if (other.gameObject.tag == "PlayerCollider")
         {
-            dragBall = true;
-            other.transform.parent.GetComponent<PlayerController>().enabled = true;
-            GetComponent<BallMagnetism>().player = other.transform.parent.gameObject;
+            dragBall = true; // activation du freinage de la balle
+            other.transform.parent.GetComponent<PlayerController>().enabled = true; // le receveur est le nouveau joueur
+            GetComponent<BallMagnetism>().player = other.transform.parent.gameObject; // la balle est aimantée à ce player
+            player = other.transform.parent.gameObject; // le nouveau player de ce script est officiellement ce player.
         }
     }
 }
