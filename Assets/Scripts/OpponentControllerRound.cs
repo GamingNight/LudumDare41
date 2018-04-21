8997d3a042cc258d6222947c3615791481dc141a
JulesPomme <julian.moreira.fr@gmail.com>
2018-04-21 10:45:52 +0200

    opponent field of view + round movement

---
+++ Assets/Scripts/OpponentControllerRound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpponentControllerRound : MonoBehaviour {
+
+    //public variables
+    public float movementFrequency = 1;
+    public bool clockwise = true;
+    public float angleStep = 90;
+
+    //private variables
+    private float timeSinceLastMove;
+
+    void Start() {
+        timeSinceLastMove = 0;
+    }
+
+    void Update() {
+
+        timeSinceLastMove += Time.deltaTime;
+        if (timeSinceLastMove >= movementFrequency) {
+            Move();
+            timeSinceLastMove = 0;
+        }
+    }
+
+    private void Move() {
+
+        transform.Rotate(0, 0, clockwise ? angleStep : -angleStep);
+    }
+}
