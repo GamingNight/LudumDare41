9d1dc09098c05307551d5fbfa61b742819b6dd67
JulesPomme <julian.moreira.fr@gmail.com>
2018-04-22 00:22:26 +0200

    ball animation

---
+++ Assets/Scripts/BallAnimationHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallAnimationHandler : MonoBehaviour {
+
+    Animator animator;
+    Rigidbody2D rgbd;
+
+    void Start() {
+        animator = GetComponent<Animator>();
+        rgbd = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        animator.SetBool("roll", rgbd.velocity.sqrMagnitude != 0 || horizontal != 0 || vertical != 0);
+        animator.SetFloat("direction", horizontal != 0 ? horizontal : vertical);
+    }
+}
