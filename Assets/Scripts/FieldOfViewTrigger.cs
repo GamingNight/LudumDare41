64667b3327788094dba2117de5c86cc45f0411fa
JulesPomme <julian.moreira.fr@gmail.com>
2018-04-21 16:18:23 +0200

    opponent attack bugged

--- Assets/Scripts/FieldOfViewTrigger.cs
+++ Assets/Scripts/FieldOfViewTrigger.cs
@@ -18,6 +18,15 @@ public class FieldOfViewTrigger : MonoBehaviour {
         if (collision.gameObject.tag == "Player") {
             playerIsDetected = true;
             spriteRenderer.color = new Color(1, 128f / 255, 128f / 255);
+            //Deactivate patrol, activate attack
+            if (transform.parent.gameObject.GetComponent<OpponentControllerPatrol>() != null)
+                transform.parent.gameObject.GetComponent<OpponentControllerPatrol>().enabled = false;
+            else if (transform.parent.gameObject.GetComponent<OpponentControllerRound>() != null)
+                transform.parent.gameObject.GetComponent<OpponentControllerRound>().enabled = false;
+            else if (transform.parent.gameObject.GetComponent<OpponentControllerScanAngle>() != null)
+                transform.parent.gameObject.GetComponent<OpponentControllerScanAngle>().enabled = false;
+
+            transform.parent.gameObject.GetComponent<OpponentControllerAttack>().enabled = true;
         }
     }
 }
---
+++ Assets/Scripts/OpponentControllerAttack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpponentControllerAttack : MonoBehaviour {
+
+    public GameObject player;
+    public float speed;
+
+    private bool attack;
+
+    private void Start() {
+
+        attack = true;
+    }
+
+    void Update() {
+
+        if (attack) {
+            //Vector2 lerpPosition = Vector2.Lerp(transform.position, player.transform.position, Time.deltaTime);
+            //transform.LookAt(lerpPosition);
+            Vector2 attackDirection = (transform.position - player.transform.position).normalized;
+            Vector3 translation = new Vector3(attackDirection.x * speed * Time.deltaTime, attackDirection.y * speed * Time.deltaTime, 0f);
+            transform.Translate(translation);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        //Stop attacking when player is reached
+        if (collision.gameObject == player) {
+            attack = false;
+        }
+    }
+}
