c20bff61121d86edb768372bec3d3e9fc5005434
JFAURE <julien.y.faure@gmail.com>
2018-04-22 21:36:51 +0200

    scoring

---
+++ Assets/Scripts/WinCalculation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCalculation : MonoBehaviour {
+
+    public float winPoints;
+	// Use this for initialization
+	void Start () {
+        winPoints = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Ball" && PlayerManager.GetInstance().ScoringPoints != 0)
+        {
+            if (PlayerManager.GetInstance().ScoringPoints > winPoints)
+            {
+                Debug.Log("YOU SCORED");
+            }
+            else
+            {
+                Debug.Log("The goal keeper catched the ball!");
+            }
+        }
+    }
+}
