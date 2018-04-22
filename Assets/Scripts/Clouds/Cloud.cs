3dcce647af7616b6d57fb6250d194865181b7530
leonaaard <contact.leo.faure@gmail.com>
2018-04-22 13:48:45 +0200

    clouds

---
+++ Assets/Scripts/Clouds/Cloud.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    public CircleCollider2D safeZone;
+
+    SpriteRenderer spriteRender;
+    private float safeZoneRadius;
+
+    void Start()
+    {
+        spriteRender = GetComponent<SpriteRenderer>();
+        safeZoneRadius = safeZone.radius;
+        transform.Rotate(0f, 0f, Random.value * 360f);
+        float randomScale = Random.value * 0.3f;
+        transform.localScale = new Vector3(1 - randomScale, 1 - randomScale, 1 - randomScale);
+    }
+
+    void Update()
+    {
+        float alpha = Mathf.Min((transform.position - safeZone.transform.position).magnitude - safeZoneRadius / 20f, safeZoneRadius) / safeZoneRadius;
+        spriteRender.color = new Color(1, 1, 1, alpha);
+    }
+}
---
+++ Assets/Scripts/Clouds/CloudManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudManager : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject cloud;
+    public CircleCollider2D safeZone;
+
+    public float spawnRadius;
+    public int cloudPerStep;
+    public int maxClouds = 1000;
+    public float spawnWait;
+
+    private List<SpriteRenderer> cloudRenderers;
+    private float bufferTotalTime;
+    private float bufferTime;
+
+    void Start()
+    {
+        bufferTotalTime = 1;
+        bufferTime = 0;
+        cloudRenderers = new List<SpriteRenderer>();
+        StartCoroutine(SpawnWaves());
+        for (int i = 0; i < maxClouds; i++)
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(player.transform.position.x - spawnRadius, player.transform.position.x + spawnRadius), Random.Range(player.transform.position.y - spawnRadius, player.transform.position.y + spawnRadius), 0f);
+            Quaternion spawnRotation = Quaternion.identity;
+            GameObject instantiateCloud = Instantiate(cloud, spawnPosition, spawnRotation);
+            instantiateCloud.transform.SetParent(transform);
+            instantiateCloud.GetComponent<Cloud>().safeZone = safeZone;
+            cloudRenderers.Add(instantiateCloud.GetComponent<SpriteRenderer>());
+        }
+    }
+
+    void Update()
+    {
+        bufferTime += Time.deltaTime;
+        if (bufferTime < bufferTotalTime)
+            return;
+        List<SpriteRenderer> _cloudRenderers = new List<SpriteRenderer>(cloudRenderers);
+        foreach (SpriteRenderer sprite in _cloudRenderers)
+        {
+            if ((sprite.transform.position - player.transform.position).magnitude > spawnRadius)
+            {
+                cloudRenderers.Remove(sprite);
+                Destroy(sprite.gameObject);
+            }
+        }
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        while (true)
+        {
+            int i = 0;
+            while (i < cloudPerStep && cloudRenderers.Count < maxClouds)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(player.transform.position.x - spawnRadius, player.transform.position.x + spawnRadius), Random.Range(player.transform.position.y - spawnRadius, player.transform.position.y + spawnRadius), 0f);
+                Quaternion spawnRotation = Quaternion.identity;
+                GameObject instantiateCloud = Instantiate(cloud, spawnPosition, spawnRotation);
+                instantiateCloud.transform.SetParent(transform);
+                instantiateCloud.GetComponent<Cloud>().safeZone = safeZone;
+                cloudRenderers.Add(instantiateCloud.GetComponent<SpriteRenderer>());
+                i++;
+            }
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+}
