28c611e19853fbe2d4dd4bf2c56d68956a0cfc5f
JulesPomme <julian.moreira.fr@gmail.com>
2018-04-23 16:30:14 +0200

    win calculation

--- Assets/Scripts/WinCalculation.cs
+++ Assets/Scripts/WinCalculation.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class WinCalculation : MonoBehaviour {
 
-    public float winPoints;
+    public float winPoints = 10;
 
     private Animator animator;
-<<<<<<< HEAD
     private bool stepBack;
     private float stepBackDuration;
     private float stepBackTimer;
@@ -27,29 +26,18 @@ public class WinCalculation : MonoBehaviour {
             float newXPos = Mathf.Lerp(transform.position.x, transform.position.x + 0.49f, stepBackTimer / stepBackDuration);
             transform.position = new Vector2(newXPos, transform.position.y);
         }
-=======
-    private Rigidbody2D rgbd;
-
-    void Start() {
-        animator = GetComponent<Animator>();
-        rgbd = GetComponent<Rigidbody2D>();
->>>>>>> 901ab146a61b23a6f8ee034c29c65550b50f7e8a
     }
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Ball" && PlayerManager.GetInstance().GetScoringPoints() != 0) {
             if (PlayerManager.GetInstance().GetScoringPoints() > winPoints) {
-<<<<<<< HEAD
                 Debug.Log("YOU SCORED");
                 other.gameObject.SetActive(false);
                 animator.SetBool("looseBall", true);
-=======
-                Debug.Log("YOU SCORED"+ PlayerManager.GetInstance().GetScoringPoints());
-                //Recul();
->>>>>>> 901ab146a61b23a6f8ee034c29c65550b50f7e8a
             } else {
+                other.gameObject.SetActive(false);
                 animator.SetBool("catchBall", true);
-                Debug.Log("The goal keeper catched the ball!" + PlayerManager.GetInstance().GetScoringPoints());
+                Debug.Log("The goal keeper catched the ball!");
             }
         }
     }
@@ -63,16 +51,8 @@ public class WinCalculation : MonoBehaviour {
 
         GameManager.GetInstance().GameOver();
     }
-<<<<<<< HEAD
 
     public void PlayerWins() {
         GameManager.GetInstance().Win();
     }
-=======
-    //void Recul()
-    //{
-    //    rgbd.velocity = new Vector2(10f, 0);
-    //    Debug.Log("pommier"+rgbd.velocity);
-    //}
->>>>>>> 901ab146a61b23a6f8ee034c29c65550b50f7e8a
 }
