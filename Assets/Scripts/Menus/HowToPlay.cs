303cda2bd7dd2c06f39f85864f3f566e095b3d8f
JulesPomme <julian.moreira.fr@gmail.com>
2018-04-23 11:44:30 +0200

    main scene

---
+++ Assets/Scripts/Menus/HowToPlay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowToPlay : MonoBehaviour {
+
+    public GameObject howToPlayerContainer;
+    public GameObject mainContainer;
+
+    public void Display() {
+        howToPlayerContainer.SetActive(true);
+        mainContainer.SetActive(false);
+    }
+}
