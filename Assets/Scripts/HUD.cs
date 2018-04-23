11fbf0a6778af8166153200929171244575e2a43
leonaaard <contact.leo.faure@gmail.com>
2018-04-23 15:08:54 +0200

    HUD

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+
+    float timeLeft = 170f;
+    public Text chrono;
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        int minutes = (int)timeLeft / 60;
+        int secondes = (int)timeLeft % 60;
+        string secondesStr = secondes + "";
+        if (secondes < 10)
+            secondesStr = "0" + secondesStr;
+        chrono.text = "0" + minutes + ":" + secondesStr;
+    }
+}
